934d8b076c60cc0bf8eabf684f35f71b71af5a6e
nouran mahmoud <nouran.mhmoud@gmail.com>
2015-12-12 14:35:38 +0200

    anything

--- assets/js/init.js
+++ assets/js/init.js
@@ -12,3 +12,23 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
 });
+
+var myApp = angular.module('myApp', []);
+myApp.controller('mainCtrl', function ($scope, $http){
+  
+  $http.get('http://api.randomuser.me/?results=24').success(function(data) {
+    $scope.users = data.results;
+  }).error(function(data, status) {
+    alert('get data error!');
+  });
+  
+  $scope.removeUser = function(user){
+     $scope.users.splice($scope.users.indexOf(user),1);
+  };
+  
+  $scope.modalDetails = function(user){
+     $scope.user = user;
+     $('#modalDetails').openModal();
+  };
+  
+});
