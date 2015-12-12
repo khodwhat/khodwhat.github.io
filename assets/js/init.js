f3647a7960a3cb3064fe810ce362bb864c10b3dc
nouran mahmoud <nouran.mhmoud@gmail.com>
2015-12-12 05:59:51 +0200

    enhance home page and messages page and profile page

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,4 +4,11 @@
     $('.button-collapse').sideNav();
 
   }); // end of document ready
+  
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+    // $('.slider').slider({full_width: true});
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+});
